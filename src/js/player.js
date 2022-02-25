66ee61ffed250c448ca28634cf80bf326b71d0c7
terjos <terjos@neuf.fr>
2022-02-25 16:32:03 +0100

    feat(global): edit JS

---
+++ src/js/player.js
@@ -0,0 +1,57 @@
+class Player {
+  constructor(id) {
+    this._container = document.getElementById(id);
+    this._score = document.getElementById(`${id}Score`);
+    this._status = document.getElementById(`${id}Status`);
+    this._roundScore = document.getElementById(`${id}RoundScore`);
+    this._state = { score: 0, roundScore: 0 };
+  }
+
+  init() {
+    this._container.classList.remove("player-end-game");
+
+    this._state = { score: 0, roundScore: 0 };
+    this._roundScore.innerHTML = 0;
+    this._score.innerHTML = 0;
+  }
+
+  win() {
+    this._status.innerHTML = "YOU WIN";
+    this._container.classList.add("player-end-game");
+  }
+
+  lose() {
+    this._status.innerHTML = "YOU LOSE";
+    this._container.classList.add("player-end-game");
+  }
+
+  active() {
+    this._container.classList.add("player-active");
+  }
+
+  inactive() {
+    this._container.classList.remove("player-active");
+  }
+
+  get score() {
+    return this._state.score;
+  }
+
+  set score(value) {
+    this._state.score = this._state.score + value;
+    this._score.innerHTML = this._state.score;
+    this._state.roundScore = 0;
+    this._roundScore.innerHTML = 0;
+  }
+
+  get roundScore() {
+    return this._state.roundScore;
+  }
+
+  set roundScore(value) {
+    this._state.roundScore = value;
+    this._roundScore.innerHTML = this._state.roundScore;
+  }
+}
+
+export default Player;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,87 @@
 import "./css/style.css";
+import Player from "./js/player";
 
-// document.querySelector("#app").innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `;
+const WIN_SCORE = 10;
+
+const btnNewGame = document.getElementById("btnNewGame");
+const btnRolldice = document.getElementById("btnRolldice");
+const btnHold = document.getElementById("btnHold");
+const imgDice = document.getElementById("dice");
+
+const firstPlayer = new Player("firstPlayer");
+const secondPlayer = new Player("secondPlayer");
+
+let hold = false;
+let currentPlayer = firstPlayer;
+let opposingPlayer = secondPlayer;
+
+currentPlayer.active();
+
+btnNewGame.addEventListener("click", handlebtnNewGame);
+btnRolldice.addEventListener("click", handleBtnRolldice);
+btnHold.addEventListener("click", handleBnHold);
+
+function handlebtnNewGame() {
+  btnHold.disabled = false;
+  btnRolldice.disabled = false;
+
+  currentPlayer.init();
+  opposingPlayer.init();
+}
+
+async function handleBtnRolldice() {
+  btnRolldice.disabled = true;
+  const dice = await rolldice();
+
+  if (hold) {
+    if (dice === 1) {
+      currentPlayer.roundScore = 0;
+      btnRolldice.disabled = false;
+    } else {
+      currentPlayer.score = currentPlayer.roundScore + dice;
+      if (currentPlayer.score >= WIN_SCORE) {
+        currentPlayer.win();
+        opposingPlayer.lose();
+        btnHold.disabled = true;
+      }
+    }
+    exchangePlayer();
+  } else {
+    currentPlayer.roundScore = dice + currentPlayer.roundScore;
+    btnRolldice.disabled = false;
+  }
+}
+
+function handleBnHold() {
+  hold = !hold;
+  btnHold.classList.toggle("btn-active");
+}
+
+function exchangePlayer() {
+  if (currentPlayer === firstPlayer) {
+    currentPlayer = secondPlayer;
+    opposingPlayer = firstPlayer;
+  } else {
+    currentPlayer = firstPlayer;
+    opposingPlayer = secondPlayer;
+  }
+  currentPlayer.active();
+  opposingPlayer.inactive();
+  hold = false;
+  btnHold.classList.remove("btn-active");
+}
+
+/**
+ * @returns {number} min 1 max 6
+ */
+async function rolldice() {
+  await sleep(300); //for animation
+
+  const dice = Math.floor(Math.random() * 6 + 1);
+  imgDice.src = `/images/dice-${dice}.png`;
+  return dice;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
